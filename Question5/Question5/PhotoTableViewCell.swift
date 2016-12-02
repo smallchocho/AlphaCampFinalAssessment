8565472784a993ff0b269359fe3ccd4b88c9f6e2
smallchocho <bigchocho@gmail.com>
2016-12-02 18:41:50 +0800

    Question5 begin

---
+++ Question5/Question5/PhotoTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoTableViewCell.swift
+//  Question5
+//
+//  Created by Justin Huang on 2016/12/2.
+//  Copyright © 2016年 Justin Huang. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+    @IBOutlet weak var photoImage: UIImageView!
+    @IBOutlet weak var dataLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Question5/Question5/PhotoTableViewController.swift
@@ -0,0 +1,106 @@
+//
+//  PhotoTableViewController.swift
+//  Question5
+//
+//  Created by Justin Huang on 2016/12/2.
+//  Copyright © 2016年 Justin Huang. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewController: UITableViewController {
+    @IBOutlet weak var takePhoto: UIBarButtonItem!
+    let photoDataBase = [
+        ["photoImage":"神劍闖江湖-京都大火篇",
+         "photoLabel":"神劍闖江湖-京都大火篇"],
+        ["photoImage":"神劍闖江湖-傳說的最終篇",
+         "photoLabel":"神劍闖江湖-傳說的最終篇"],
+        ["photoImage":"模仿遊戲",
+         "photoLabel":"模仿遊戲"],
+    ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return photoDataBase.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PhotoTableViewCell
+        cell.photoImage.image = UIImage(named: photoDataBase[indexPath.row]["photoImage"]!)
+        cell.dataLabel.text = photoDataBase[indexPath.row]["photoLabel"]
+
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
